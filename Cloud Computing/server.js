2a4752464f19b75b049a27a930313bd6276d0726
nazwan14 <101103321+nazwan14@users.noreply.github.com>
2023-12-17 20:09:06 +0700

    Update server.js

--- Cloud Computing/server.js
+++ Cloud Computing/server.js
@@ -1,8 +1,12 @@
+
 const express = require("express");
 const app = express();
-
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const tf = require('@tensorflow/tfjs');
 const admin = require("firebase-admin");
 const credentials = require("./serviceAccountKey.json");
+
 
 admin.initializeApp({
     credential: admin.credential.cert(credentials)
@@ -74,6 +78,43 @@ app.delete('/delete/:id', async (req, res) => {
         const response = await db.collection("users").doc(req.params.id).delete();
         res.send(response);
     }   catch(error) {
+        res.send(error);
+    }
+});
+
+// Signup route
+app.post('/signup', async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
+        const userJson = {
+            email: req.body.email,
+            name: req.body.name,
+            password: hashedPassword,
+            // add other fields as necessary
+        };
+        const response = await db.collection("users").add(userJson);
+        res.send(response.id);
+    } catch(error) {
+        res.send(error);
+    }
+});
+
+// Login route
+app.post('/login', async (req, res) => {
+    try {
+        const userSnapshot = await db.collection("users").where('email', '==', req.body.email).get();
+        if (userSnapshot.empty) {
+            res.status(400).send('Invalid email or password');
+            return;
+        }
+        const userData = userSnapshot.docs[0].data();
+        const passwordMatch = await bcrypt.compare(req.body.password, userData.password);
+        if (!passwordMatch) {
+            res.status(400).send('Invalid email or password');
+            return;
+        }
+        res.send('Logged in successfully');
+    } catch(error) {
         res.send(error);
     }
 });
