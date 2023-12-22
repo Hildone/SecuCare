0186e3630e673af9cf2aad729f21fc0b2b659d23
Hilal Haidar <81667329+Hildone@users.noreply.github.com>
2023-12-22 15:25:47 +0700

    Update Prediction.py

--- Machine Learning/Prediction.py
+++ Machine Learning/Prediction.py
@@ -10,7 +10,7 @@ seconds = 2
 filename = "prediction.wav"
 
 ##### LOADING OUR SAVED MODEL and PREDICTING ###
-model = load_model("saved_model/WakeWord.h5")
+model = load_model("saved_model/Model.h5")
 
 print("Prediksi Dimulai: ")
 i = 0
@@ -26,12 +26,12 @@ while True:
 
     prediction = model.predict(np.expand_dims(mfcc_processed, axis=0))
     if prediction[:, 1] > 0.99:
-        print(f"Wake Word Detected for ({i})")
+        print(f"Tolong Detected ({i})")
         print("Confidence:", prediction[:, 1])
         i += 1
     
     else:
-        print(f"Wake Word NOT Detected")
+        print(f"Tolong Not Detected")
         print("Confidence:", prediction[:, 0])
     
     input(f"Enter")
